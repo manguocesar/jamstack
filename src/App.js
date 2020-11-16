4ca5708375beacceaa354183da02b38f51b12f9e
cesar <cesar.hertz@icloud.com>
2020-11-16 16:32:14 +0800

    first one

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useEffect, useState } from 'react';
+import LinkList from './components/LinkList';
+import LinkForm from './components/LinkForm';
+//Grab all of the links
+//display all of the links
+//add delete and archive functionality
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [links, setLinks] = useState([]);
+    const loadLinks = async () => {
+        try {
+            const res = await fetch('/.netlify/functions/getLinks');
+            const links = await res.json();
+            setLinks(links);
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
+    useEffect(() => {
+        loadLinks();
+    }, []);
+
+    return (
+        <div className="container py-5">
+            <h1 className="text-center mb-5">List O' Link</h1>
+            <LinkForm refreshLinks={loadLinks} />
+            <LinkList links={links} refreshLinks={loadLinks} />
+        </div>
+    );
 }
 
 export default App;
